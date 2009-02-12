a8ea560448b688c1eadaca6275724b307d9d93dc
FND <fnd@fnd-osmosoft.(none)>
2009-02-12 16:14:09 +0000

    added basic Python module template
    adapted from David Goodger (http://python.net/~goodger/projects/pycon/2007/idiomatic/)

---
+++ module.py
@@ -0,0 +1,30 @@
+"""
+<module description>
+"""
+
+
+import sys
+
+
+# <constants>
+
+
+# <exception classes>
+
+
+# <interface functions>
+
+
+# <classes>
+
+
+# <internal functions & classes>
+
+
+def main(argv=None):
+	return True
+
+
+if __name__ == "__main__":
+	status = not main(sys.argv) # convert boolean
+	sys.exit(status)
